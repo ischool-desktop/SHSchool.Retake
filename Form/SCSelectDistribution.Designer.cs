41e509b8c5835d1aac8b38fbbaa305f9e876b8e9
lelala <lelala@ischool.com.tw>
2016-12-08 12:03:05 +0800

    分發功能半成品

---
+++ Form/SCSelectDistribution.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace SHSchool.Retake.Form
+{
+    partial class SCSelectDistribution
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSave = new DevComponents.DotNetBar.ButtonX();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSave.AutoSize = true;
+            this.btnSave.BackColor = System.Drawing.Color.Transparent;
+            this.btnSave.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.btnSave.Location = new System.Drawing.Point(310, 261);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(82, 25);
+            this.btnSave.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "分發";
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // SCSelectDistribution
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 298);
+            this.Controls.Add(this.btnSave);
+            this.DoubleBuffered = true;
+            this.Name = "SCSelectDistribution";
+            this.Text = "課程自動分發";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevComponents.DotNetBar.ButtonX btnSave;
+    }
+}
---
+++ Form/SCSelectDistribution.cs
@@ -0,0 +1,163 @@
+﻿using FISCA.UDT;
+using SHSchool.Retake.DAO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SHSchool.Retake.Form
+{
+    public partial class SCSelectDistribution : FISCA.Presentation.Controls.BaseForm
+    {
+        public SCSelectDistribution()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            BackgroundWorker bkw = new BackgroundWorker() { WorkerReportsProgress = true };
+            bkw.DoWork += delegate
+            {
+                AccessHelper accessHepler = new AccessHelper();
+                #region row 欄位
+                //dept.stu_dept, class.grade_year, class.class_name, student.seat_no, student.student_number, student.name
+                //, scourse.subject_name, scourse.subject_level, scourse.credit
+                //, session.school_year, session.semester, session.round
+                //, CASE WHEN cscourse.subject_type is null THEN sscourse.subject_type ELSE cscourse.subject_type END
+                //, cscourse.course_name
+                //, sscourse.uid as select_subject_id, cscourse.uid as attend_course_id, student as student_id 
+                #endregion
+                Dictionary<string, List<DataRow>> dicStudentSelect = new Dictionary<string, List<DataRow>>();
+                #region 整理取得學生須分發的選課紀錄
+                {
+                    FISCA.Data.QueryHelper queryHelper = new FISCA.Data.QueryHelper();
+                    var dt = queryHelper.Select(@"
+SELECT dept.stu_dept, class.grade_year, class.class_name, student.seat_no, student.student_number, student.name
+	, scourse.subject_name, scourse.subject_level, scourse.credit
+	, session.school_year, session.semester, session.round
+	, CASE WHEN cscourse.subject_type is null THEN sscourse.subject_type ELSE cscourse.subject_type END
+	, cscourse.course_name
+	, sscourse.uid as select_subject_id, cscourse.uid as attend_course_id, student as student_id
+FROM (
+		SELECT csselect.ref_student_id, course.subject_name, course.subject_level, course.credit, course.school_year, course.semester, course.round
+		FROM $shschool.retake.scselect csselect
+			LEFT OUTER JOIN $shschool.retake.course course on course.uid = csselect.ref_course_id
+		UNION
+		SELECT ssselect.ref_student_id, subject.subject_name, subject.subject_level, subject.credit, subject.school_year, subject.semester, subject.round
+		FROM $shschool.retake.ssselect ssselect
+			LEFT OUTER JOIN $shschool.retake.subject subject on ssselect.ref_subject_id = subject.uid
+	) as scourse
+	LEFt OUTER JOIN student on student.id = scourse.ref_student_id
+	LEFt OUTER JOIN class on class.id = student.ref_class_id
+	LEFT OUTER JOIN (
+		SELECT s.id as ref_student_id, CASE WHEN d1.name is null THEN d2.name ELSE d1.name END as stu_dept
+		FROM student s
+			LEFT OUTER JOIN class c on c.id = s.ref_class_id
+			LEFT OUTER JOIN dept d1 on d1.id = s.ref_dept_id
+			LEFT OUTER JOIN dept d2 on d2.id = c.ref_dept_id
+	) as dept on student.id = dept.ref_student_id
+	LEFT OUTER JOIN $shschool.retake.session session on scourse.school_year=session.school_year and scourse.semester=session.semester and scourse.round=session.round
+	LEFT OUTER JOIN (
+		SELECT ssselect.ref_student_id, subject.subject_name, subject.subject_level, subject.credit, subject.school_year, subject.semester, subject.round
+			, subject.subject_type, subject.uid
+		FROM $shschool.retake.ssselect ssselect
+			LEFT OUTER JOIN $shschool.retake.subject subject on ssselect.ref_subject_id = subject.uid
+	) as sscourse on sscourse.ref_student_id = scourse.ref_student_id 
+		AND sscourse.subject_name = scourse.subject_name 
+		AND (
+			sscourse.subject_level = scourse.subject_level  
+			OR (
+				sscourse.subject_level is null
+				AND
+				scourse.subject_level is null
+			)
+		)
+		AND sscourse.credit = scourse.credit
+		AND sscourse.school_year = scourse.school_year
+		AND sscourse.semester = scourse.semester
+		AND sscourse.round = scourse.round 
+	LEFT OUTER JOIN (
+		SELECT csselect.ref_student_id, course.subject_name, course.subject_level, course.credit, course.school_year, course.semester, course.round
+			, course.course_name, course.subject_type, course.uid
+		FROM $shschool.retake.scselect csselect
+			LEFT OUTER JOIN $shschool.retake.course course on course.uid = csselect.ref_course_id
+	) as cscourse on cscourse.ref_student_id = scourse.ref_student_id 
+		AND cscourse.subject_name = scourse.subject_name 
+		AND (
+			cscourse.subject_level = scourse.subject_level  
+			OR (
+				cscourse.subject_level is null
+				AND
+				scourse.subject_level is null
+			)
+		)
+		AND cscourse.credit = scourse.credit
+		AND cscourse.school_year = scourse.school_year
+		AND cscourse.semester = scourse.semester
+		AND cscourse.round = scourse.round
+WHERE 
+	session.active = true
+    AND sscourse.uid is not null
+    AND cscourse.uid is null
+ORDER BY session.school_year desc, session.semester desc, session.round desc, stu_dept, class.grade_year desc, class.display_order, class.class_name, student.seat_no, student.id, scourse.credit desc, scourse.subject_name, scourse.subject_level
+");
+
+                    foreach (System.Data.DataRow row in dt.Rows)
+                    {
+                        var studentID = "" + row["student_id"];
+                        if (!dicStudentSelect.ContainsKey(studentID))
+                        {
+                            dicStudentSelect.Add(studentID, new List<DataRow>());
+                        }
+                        dicStudentSelect[studentID].Add(row);
+                    }
+                }
+                #endregion
+
+                Dictionary<string, List<DataRow>> dicCourseList = new Dictionary<string, List<DataRow>>();
+                #region 整理課程資料
+                {
+                    FISCA.Data.QueryHelper queryHelper = new FISCA.Data.QueryHelper();
+                    var dt = queryHelper.Select(@"
+SELECT course.uid, course.subject_name, course.subject_level, course.credit
+    , $shschool.retake.cdselect.dept_name as dept_name, scount.student_count
+FROM $shschool.retake.course course
+	LEFT OUTER JOIN $shschool.retake.session session on course.school_year = session.school_year AND course.semester = session.semester AND course.round = session.round
+	LEFT OUTER JOIN $shschool.retake.cdselect on $shschool.retake.cdselect.ref_course_timetable_id = course.course_timetable_id
+	LEFT OUTER JOIN (
+		SELECT ref_course_id, count(*) as student_count
+		FROM $shschool.retake.scselect
+		GROUP BY ref_course_id
+	) as scount on scount.ref_course_id = course.uid
+WHERE
+	session.active = true
+ORDER BY subject_name, subject_level, credit
+");
+
+                    foreach (System.Data.DataRow row in dt.Rows)
+                    {
+                        var key = "" + row["subject_name"] + "^^" + row["subject_level"] + "^^" + row["credit"] + "^^" + row["dept_name"];
+                        if (!dicCourseList.ContainsKey(key))
+                        {
+                            dicCourseList.Add(key, new List<DataRow>());
+                        }
+                        dicCourseList[key].Add(row);
+                    }
+                }
+                #endregion
+
+
+            };
+            bkw.RunWorkerCompleted += delegate
+                {
+                    FISCA.Presentation.Controls.MsgBox.Show("分發完成");
+                };
+            bkw.RunWorkerAsync();
+        }
+    }
+}
